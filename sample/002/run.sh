c58d40f84707ded5c1b287b00b048a0f7cdb014c
hell0again <hell00again@gmail.com>
2015-05-21 17:53:12 +0900

    [master] fix sample

--- sample/002/run.sh
+++ sample/002/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd ${SCRIPT_DIR}/../..
-#tableconv conv ${SCRIPT_DIR}/source.csv > ${SCRIPT_DIR}/conv.xlsx
-#tableconv reverse --ff=unix ${SCRIPT_DIR}/conv.xlsx > ${SCRIPT_DIR}/out.csv
-carton exec perl -d -Ilib ./script/tableconv conv ${SCRIPT_DIR}/source.csv > ${SCRIPT_DIR}/conv.xlsx
-carton exec perl -d -Ilib ./script/tableconv reverse --ff=unix ${SCRIPT_DIR}/conv.xlsx > ${SCRIPT_DIR}/out.csv
+tableconv conv ${SCRIPT_DIR}/source.csv > ${SCRIPT_DIR}/conv.xlsx
+tableconv reverse --ff=unix ${SCRIPT_DIR}/conv.xlsx > ${SCRIPT_DIR}/out.csv
+#carton exec perl -d -Ilib ./script/tableconv conv ${SCRIPT_DIR}/source.csv > ${SCRIPT_DIR}/conv.xlsx
+#carton exec perl -d -Ilib ./script/tableconv reverse --ff=unix ${SCRIPT_DIR}/conv.xlsx > ${SCRIPT_DIR}/out.csv
 diff -u ${SCRIPT_DIR}/{source,out}.csv
 
